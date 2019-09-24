4f496969e0ae6afce1c52c3c6fc62987345cecf7
Mahmoud Abd Elghany <17260098+Mahmoud-Abd-Elghany@users.noreply.github.com>
2019-09-24 14:34:12 +0200

    v0.02

--- ICU.h
+++ ICU.h
@@ -26,6 +26,35 @@ typedef struct
 	clock prescaler;
 	edge edgetype;
 
-}config;
+}ICU_config;
+
+/*Description:
+ * Function responible for initializing ICU
+ * 1- Set prescaler
+ * 2- Set Edge type at which you want to capture the value
+ */
+void ICU_init (const ICU_config *);
+
+/*Description:
+ *Function used to call another function after finishing the task
+ */
+void ICU_SetCallBack(void (*a_Fun_Ptr)(void));
+
+/*Description:
+ *Used to return value of ICU captured value
+ */
+uint16 ICU_getInputValue(void);
+
+/*Description:
+ *Change the edge at whch you want to capture value
+ */
+void ICU_SetEdgeDetectionType(edge);
+
+/*Description:
+ *Stop ICU
+ */
+void ICU_DeInit(void);
+
+
 
 #endif /* ICU_H_ */
